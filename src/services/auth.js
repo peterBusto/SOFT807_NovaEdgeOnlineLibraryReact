63bd9fbad3c5f5621bce2ebb0b91d9af883a780c
Peter <pebu001@ess.ais.ac.nz>
2026-07-13 02:20:07 +1200

    updated the auth file

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000';
-
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
